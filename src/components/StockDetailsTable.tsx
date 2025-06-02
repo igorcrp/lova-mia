abce1852b2a46ecd89d175b19faf9c4297375be1
igorcrp <35533139+igorcrp@users.noreply.github.com>
2025-06-02 14:15:20 -0300

    Update StockDetailsTable.tsx

--- src/components/StockDetailsTable.tsx
+++ src/components/StockDetailsTable.tsx
@@ -430,7 +430,7 @@ export function StockDetailsTable({
                           formattedValue = item.stopTrigger || "-";
                         } else if (column.id === "trade") {
                           // Conditionally display 'Executed' for daytrade interval
-                          formattedValue = params.interval === 'daytrade' && String(value) === 'Buy' ? 'Executed' : String(value);
+                          formattedValue = params.interval === 'daytrade' && String(value) === 'Executed' ? 'Executed' : String(value);
                         } else if (typeof value === "number") {
                           formattedValue = value.toFixed(2);
                         } else {
