eac46f3a4c2acd24f6ded4044ef04c0585a23ad7
igorcrp <35533139+igorcrp@users.noreply.github.com>
2025-06-05 15:58:13 -0300

    Update StockDetailsTable.tsx

--- src/components/StockDetailsTable.tsx
+++ src/components/StockDetailsTable.tsx
@@ -567,11 +567,10 @@ export function StockDetailsTable({
               <SelectValue placeholder={itemsPerPage} />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="5">5</SelectItem>
               <SelectItem value="10">10</SelectItem>
-              <SelectItem value="20">20</SelectItem>
               <SelectItem value="50">50</SelectItem>
               <SelectItem value="100">100</SelectItem>
+              <SelectItem value="500">500</SelectItem>
             </SelectContent>
           </Select>
         </div>
